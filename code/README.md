ea9a35e90cce9aa32dd1b0188256b4f15040a7cb
mavallejos93 <mavallejos93@gmail.com>
2023-09-25 16:03:14 -0500

    code lab

---
+++ code/README.md
@@ -0,0 +1,10 @@
+# employee-management-dmss
+
+This is a Full Stack CRUD Application for DBMS subject project.
+
+This project is based on https://github.com/KG-1510/employee-management-dbms/tree/main.
+
+this project  contains new configurations to support environment variables and to be able to run it on kubernetes. 
+
+
+
---
+++ code/backend/index.js
@@ -0,0 +1,90 @@
+require('dotenv').config()
+
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
+
+const db = mysql.createPool({
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+app.use(express.json());
+
+db.query("CREATE TABLE employees(name  VARCHAR(50) NOT NULL, age INT, country VARCHAR(256) NOT NULL, position VARCHAR(256),  wage INT ,PRIMARY KEY(position))", (err, result) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(result);
+  }
+});
+
+
+app.post("/create", (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
+
+  db.query(
+    "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+    [name, age, country, position, wage],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
+});
+
+app.get("/employees", (req, res) => {
+  db.query("SELECT * FROM employees", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.put("/update", (req, res) => {
+  const id = req.body.id;
+  const wage = req.body.wage;
+  db.query(
+    "UPDATE employees SET wage = ? WHERE id = ?",
+    [wage, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("✅ Server running on port: 3001");
+});
