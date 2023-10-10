24a0e2caac0047b03b43c0458634de5076a1ff01
mavallejos93 <mavallejos93@gmail.com>
2023-10-10 13:38:37 -0500

    commands and version mariadb

--- infra/commands.sh
+++ infra/commands.sh
@@ -1,11 +1,19 @@
+############################## create images docker ##############################
+docker build . -t tech-backend:v1.0 
+
+docker build . -t tech-frontend:v1.0 
+
+Docker image ls 
+
+############################## install datadog with helm ##############################
 helm repo add datadog https://helm.datadoghq.com
 
-helm install datadog-values.yaml datadogtest datadog/datadog \
-  --set datadog.site='datadoghq.com' \
-  --set datadog.apiKey='2df52e2c0aa3cfe8f77a37e2a98eb8ad' \
-  --set clusterAgent.replicas='2' \
-  --set clusterAgent.createPodDisruptionBudget=true
-
+helm install datadogtest -f datadog-values.yaml \
+    --set datadog.site='us5.datadoghq.com' \
+    --set datadog.apiKey='4eb6dfdc49ff926ebec6d3deec9e9202' \
+    --set clusterAgent.replicas='2' \
+    --set clusterAgent.createPodDisruptionBudget=true \
+    datadog/datadog
 
 helm upgrade datadogtest -f datadog-values.yaml \
     --set datadog.site='us5.datadoghq.com' \
